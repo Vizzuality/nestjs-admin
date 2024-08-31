de9ec4aed3e39fb79b1cad00efd01181d6bc4ee6
alexeh <alexurola@gmail.com>
2024-08-31 11:28:08 +0200

    add import-export feature to users

--- src/modules/adminmodule/admin.module.ts
+++ src/modules/adminmodule/admin.module.ts
@@ -1,15 +1,19 @@
 import { Global, Module } from '@nestjs/common';
 import { AdminModule } from '@adminjs/nestjs';
-import AdminJS from 'adminjs';
+import AdminJS, { ComponentLoader } from 'adminjs';
 import * as AdminJSTypeorm from '@adminjs/typeorm';
 import { User } from '../../entities/user.entity.js';
 import { Post } from '../../entities/post.entity.js';
 import { Comment } from '../../entities/comment.entity.js';
+import * as process from 'node:process';
+import importExportFeature from '@adminjs/import-export';
 
 AdminJS.registerAdapter({
   Database: AdminJSTypeorm.Database,
   Resource: AdminJSTypeorm.Resource,
 });
+
+const componentLoader = new ComponentLoader();
 
 @Global()
 @Module({
@@ -19,15 +23,23 @@ AdminJS.registerAdapter({
         return {
           adminJsOptions: {
             rootPath: '/admin',
+            componentLoader,
             // Rename "organizations" to your table name or set "resources" to []
-            resources: [User, Post, Comment],
+            resources: [
+              {
+                resource: User,
+                features: [importExportFeature({ componentLoader })],
+              },
+              { resource: Post },
+              { resource: Comment },
+            ],
           },
           auth: {
             authenticate: async (email, password) => {
               return { email };
             },
-            cookiePassword: 'secret',
-            cookieName: 'adminjs',
+            cookiePassword: process.env.USERNAME,
+            cookieName: process.env.PASSWORD,
           },
           sessionOptions: {
             resave: true,
