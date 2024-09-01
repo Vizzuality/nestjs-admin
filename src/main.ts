0aca678333b99feb5dc7665a10113d13884a4be0
alexeh <alexurola@gmail.com>
2024-09-01 07:13:17 +0200

    specify hostname in entrypoint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 
+const PORT = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT, '0.0.0.0');
 }
 bootstrap();
